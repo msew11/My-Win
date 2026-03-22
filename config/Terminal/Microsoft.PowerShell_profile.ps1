75d641e09865531bd722f799a9bfc59412837b8b
msew11 <635481671@qq.com>
2026-03-22 22:06:13 +0800

    修改powershell脚本

--- config/Terminal/Microsoft.PowerShell_profile.ps1
+++ config/Terminal/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,13 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-
 Import-Module -Name Terminal-Icons
 
 fastfetch --logo-position top
+
+function y {
+	$tmp = (New-TemporaryFile).FullName
+	yazi.exe $args --cwd-file="$tmp"
+	$cwd = Get-Content -Path $tmp -Encoding UTF8
+	if ($cwd -and $cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+		Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+	}
+	Remove-Item -Path $tmp
+}
