bae1f76e5304ce8ac62a2e41812e3ff4d08749f7
msew11 <635481671@qq.com>
2026-03-22 16:16:59 +0800

    统一git相关插件显示文件状态的图标

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,21 +1,24 @@
 th.git = th.git or {}
-th.git.modified = ui.Style():fg("blue")
-th.git.deleted = ui.Style():fg("red"):bold()
-th.git.unknown_sign = " "
-th.git.modified_sign = "M"
-th.git.deleted_sign = "D"
-th.git.clean_sign = "✔"
+th.git.modified = ui.Style():fg("yellow")
+th.git.deleted = ui.Style():fg("red")
+th.git.added = ui.Style():fg("green")
+th.git.untracked = ui.Style():fg("magenta")
+th.git.ignored = ui.Style():fg("gray")
 
+th.git.unknown_sign = " "
+th.git.modified_sign = " "
+th.git.deleted_sign = " "
+th.git.added_sign = " "
+th.git.untracked_sign = " "
+th.git.ignored_sign = " "
+th.git.clean_sign = ""
+
+-- 官方 Git 插件：显示文件列表右侧状态 (Linemode)
 require("git"):setup {
-	-- Order of status signs showing in the linemode
 	order = 1500,
 }
 
-require("full-border"):setup {
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
-}
-
+-- Githead 插件：显示顶部 Header 的分支和详细统计
 require("githead"):setup({
   order = {
     "__spacer__",
@@ -39,34 +42,39 @@ require("githead"):setup({
     "behind_ahead_remote",
     "__spacer__",
   },
-
-  branch_borders = "{}",
-  branch_prefix = "|",
+  branch_borders = "()",
+  branch_prefix = "on ",
+  branch_symbol = " ",
   branch_color = "#7aa2f7",
   remote_branch_color = "#9ece6a",
   always_show_remote_branch = true,
   always_show_remote_repo = true,
 
-  tag_symbol = "󰓼",
+  tag_symbol = "󰓼 ",
   always_show_tag = true,
   tag_color = "#bb9af7",
 
-  commit_symbol = "",
+  commit_symbol = " ",
   always_show_commit = true,
   commit_color = "#e0af68",
 
   staged_color = "#73daca",
-  staged_symbol = "●",
+  staged_symbol = " ",
 
   unstaged_color = "#e0af68",
-  unstaged_symbol = "✗",
+  unstaged_symbol = " ",
 
   untracked_color = "#f7768e",
-  untracked_symbol = "?",
+  untracked_symbol = " ",
 
   state_color = "#f5c359",
-  state_symbol = "󱐋",
+  state_symbol = "󱐋 ",
 
   stashes_color = "#565f89",
-  stashes_symbol = "⚑",
+  stashes_symbol = "⚑ ",
 })
+
+-- 全边框 UI
+require("full-border"):setup {
+	type = ui.Border.ROUNDED,
+}
